bc3b2fa8f0ed2654100583ac1251e7cf791e7c35
Manuela Bognar <manuelabcamara@gmail.com>
2021-08-12 22:08:58 -0300

    add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
     <>
-      <h1>Hello world!</h1>
-      <h2>with nextJS!</h2>
+      <h1 className={styles.title}>Hello world with nextjs and <span>sass</span></h1>
     </>
   )
 }
