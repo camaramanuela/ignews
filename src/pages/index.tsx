89074c9b4396a544093054850cc6ee6bc709fdb9
Manuela Bognar <manuelabcamara@gmail.com>
2021-08-12 22:27:05 -0300

    fonts config; dinamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title> Home | Ignews </title>
+      </Head>
       <h1 className={styles.title}>Hello world with nextjs and <span>sass</span></h1>
     </>
   )
