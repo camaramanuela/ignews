8e1ad3b952a12f8cf87d8860df12c940b647c120
Manuela Bognar <manuelabcamara@gmail.com>
2021-08-16 22:32:28 -0300

    listing posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,43 +1,41 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
-import { stringify } from 'querystring';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return(
     <>
-    <Head>
-      <title>Posts | Ignews</title>
-    </Head>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
 
-    <main className={styles.container}>
-      <div className={styles.posts}>
-        <a href="#">
-          <time>12 de março de 2021</time>
-          <strong>Creating a Monorepo with Lernasadoasko</strong>
-          <p>in this guide... dasidjais jdijasi jdiasjdi as d</p>
-        </a>
-        <a href="#">
-          <time>12 de março de 2021</time>
-          <strong>Creating a Monorepo with Lernasadoasko d</strong>
-          <p>in this guide... dasidjais jdijasi jdiasjdi as d</p>
-        </a>
-        <a href="#">
-          <time>12 de março de 2021</time>
-          <strong>Creating a Monorepo with Lernasadoasko d</strong>
-          <p>in this guide... dasidjais jdijasi jdiasjdi as d</p>
-        </a>
-        <a href="#">
-          <time>12 de março de 2021</time>
-          <strong>Creating a Monorepo with Lernasadoasko d</strong>
-          <p>in this guide... dasidjais jdijasi jdiasjdi as d</p>
-        </a>
-      </div>
-    </main>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          { posts.map(post => (
+          <a key={post.slug} href="#">
+            <time>{post.updatedAt}</time>
+            <strong>{post.title}</strong>
+            <p>{post.excerpt}</p>
+            </a>
+          ))}
+        </div>
+      </main>
     </>
   )
 }
@@ -52,9 +50,20 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
 
   return {
-    props: {}
+    props: {posts}
   }
 }
